7986b4cc616b4d501e1ee45b2e127155b346c27b
TaoismLEE <564735984@qq.com>
2021-10-28 09:39:03 +0800

    优化请求发送类

--- lib/send_requests.py
+++ lib/send_requests.py
@@ -3,35 +3,41 @@
 
 class SendRequests():
     """发送请求数据"""
-    def sendRequests(self, s, apiData):
+    def send_request(self, s, api_data):
         try:
             # 从读取的表格中获取响应的参数作为传递
-            method = apiData["method"]
-            url = apiData["url"]
+            method = api_data["method"]
+            url = api_data["url"]
 
-            if apiData["params"] == "":
+            if api_data["params"] == "":
                 params = None
             else:
-                params = eval(apiData["params"])
+                params = eval(api_data["params"])
 
-            if apiData["headers"] == "":
+            if api_data["headers"] == "":
                 headers = None
             else:
-                headers = eval(apiData["headers"])
+                headers = eval(api_data["headers"])
 
-            if apiData["body"] == "":
+            if api_data["body"] == "":
                 body_data = None
             else:
-                body_data = eval(apiData["body"])
+                body_data = api_data["body"]
 
-            type = apiData["type"]
-            if type == "data":
+            data_type = api_data["type"]
+            if data_type == "data":
+                body = eval(body_data)
+            elif data_type == "json":
                 body = body_data
             else:
                 body = params
 
             # 发送请求
-            re = s.request(method=method, url=url, headers=headers, data=body)
+            if data_type == "json":
+                headers['Content-Type'] = 'application/json;charset=UTF-8'
+                re = s.request(method=method, url=url, headers=headers, data=body.encode('utf-8'))
+            else:
+                re = s.request(method=method, url=url, headers=headers, data=body)
             return re
         except Exception as e:
             print(e)
